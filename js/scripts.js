d45c680b61757832e9b8400836b2a072423203f9
shankbansal <50798143+shankbansal@users.noreply.github.com>
2019-05-31 16:24:31 +0200

    added reposotory.pokemon
    added 3 pokemon with keys and added loop for heading names, conditional loop for height chractreristics.
    
    also added styiling with css (remove later)

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,27 @@
-alert('Hello world');
+var repository = [
+  {
+    name: 'Bulbasaur',
+    height:7,
+    type: ['grass','poison']
+  },
+  {
+    name: 'Pikachu',
+    height:4,
+    type: ['electric']
+  },
+  {
+    name: 'Squirtle',
+    height:5,
+    type: ['water']
+  }
+];
+
+for(i = 0; i<repository.length; i++) {
+  console.log(repository[i].name);
+  document.write('<p id = "title"> My name is:</p><h1>' + repository[i].name + '</h1>' + '<h1 id = "height">'+ 'Height: ' + repository[i].height + '</h1>' )
+  if (repository[i].height>5) { document.write('<p>wow! what a big Pokemon!</p>');
+
+} else { document.write('<p>thats cute!</p>');
+
+  }
+}
