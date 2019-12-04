202871e161a00d65c745f7ec71f2625bfe87e96e
shankbansal <50798143+shankbansal@users.noreply.github.com>
2019-12-04 15:10:32 +0100

    added es lint and made one js file

--- js/custom/script.js
+++ js/custom/script.js
@@ -8,6 +8,22 @@ var modalHeight = document.getElementById("modal-height");
 var modalWeight = document.getElementById("modal-weight");
 var modalImage = document.getElementById("modal-image");
 
+const apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=100';
+
+/* Get Pokemon list */
+async function getPokemonList() {
+    let response = await fetch(apiUrl);
+    response = response.json();
+    return response
+}
+
+/* Get Pokemon details */
+async function getPokemonDetail(url) {
+    let response = await fetch(url);
+    response = response.json();
+    return response
+}
+
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
